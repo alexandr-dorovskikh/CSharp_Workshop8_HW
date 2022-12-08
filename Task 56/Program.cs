288d512fd5bf754ccee137300ce69657f4660a11
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-08 21:45:22 +0700

    Task 56 added

---
+++ Task 56/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillRandomValuesInArray(ref int[,] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+        for (int j = 0; j < NumsArray.GetLength(1); j++)
+            NumsArray[i, j] = new Random().Next(0, 10);
+
+
+}
+
+void PrintArray(int[,] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < NumsArray.GetLength(1); j++)
+            Console.Write($"{NumsArray[i, j]} \t");
+
+        Console.WriteLine("");
+    }
+}
+
+int LineWithMinSum(int[,] NumsArray)
+{
+    int? MinSum = null;
+    int Result = 0;
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {    
+        int LineSum = 0;
+        
+        for (int j = 0; j < NumsArray.GetLength(1); j++)
+            LineSum += NumsArray[i, j];
+        
+        if (MinSum == null || LineSum < MinSum) 
+        {
+            MinSum = LineSum;
+            Result = i + 1;
+        };
+    }
+
+    return Result;        
+}
+
+int m = 5;
+int n = 5;
+
+int[,] NumsArray = new int[m, n];
+FillRandomValuesInArray(ref NumsArray);
+PrintArray(NumsArray);
+
+Console.WriteLine($"Line with min sum: {LineWithMinSum(NumsArray)}");
